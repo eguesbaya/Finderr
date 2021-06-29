8d6996e7a8603551f51b63b88c64cdad3b8a9cee
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-06-29 20:03:40 +0200

    changement type private  en ?Collection

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -14,6 +15,7 @@ use DateTime;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -99,7 +101,7 @@ class User implements UserInterface
     /**
      * @ORM\ManyToOne(targetEntity=Skill::class, inversedBy="users")
      */
-    private ?Skill $skill;
+    private ?Collection $skill;
 
 
 
