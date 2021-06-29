d4683f33b28c3070ec989cd9cfaad199f05cf914
aurelien <vannier.autrelien2@gmail.com>
2021-06-29 13:12:31 +0200

    modif fixture user

--- src/DataFixtures/SexFixtures.php
+++ src/DataFixtures/SexFixtures.php
@@ -16,7 +16,7 @@ class SexFixtures extends Fixture
             $sex = new Sex();
             $sex->setName($sexName);
             $manager->persist($sex);
-            $this->addReference('sex' . $key, $sex);
+            $this->addReference('sex_' . $key, $sex);
         }
         $manager->flush();
     }
